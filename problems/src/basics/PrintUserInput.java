a7dc562ae0e8ff770b87bea20f0bebc4e52f00cc
--replace-all <sohit.manjul@gmail.com>
2025-12-05 23:19:38 +0530

    print user input

---
+++ problems/src/basics/PrintUserInput.java
@@ -0,0 +1,31 @@
+package basics;
+
+import java.util.Scanner;
+
+class Solution {
+    // Function to take input and display output
+    public void printNumber(Scanner sc) {
+
+        int number;
+
+        // Take input
+        number = sc.nextInt();
+
+        // Print output
+        System.out.print(number);
+    }
+}
+
+// Driver code
+class Main {
+    public static void main(String[] args) {
+        // Creating an instance of Solution class
+        Solution sol = new Solution();
+
+        // Scanner class
+        Scanner sc = new Scanner(System.in);
+
+        // Function call to take input and display output
+        sol.printNumber(sc);
+    }
+}
